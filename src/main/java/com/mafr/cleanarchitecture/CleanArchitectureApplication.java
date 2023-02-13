fccea2a8d7a91a04e56c8c17e32b7d3fbbc38a26
Muhammad Aditya Fathur Rahman <madityafr3@gmail.com>
2023-02-13 23:05:33 +0700

    hiyaa

--- src/main/java/com/mafr/cleanarchitecture/CleanArchitectureApplication.java
+++ src/main/java/com/mafr/cleanarchitecture/CleanArchitectureApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CleanArchitectureApplication {
-
+    // Hiyaa
     public static void main(String[] args) {
         SpringApplication.run(CleanArchitectureApplication.class, args);
     }
